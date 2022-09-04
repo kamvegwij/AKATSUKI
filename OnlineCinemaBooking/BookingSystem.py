75495903f79fb1b9ccd5b618c8b28deeba06331c
snare0625 <86784170+snare0625@users.noreply.github.com>
2022-09-04 23:49:39 +0200

    Added Register form and Login form validation classes

--- OnlineCinemaBooking/BookingSystem.py
+++ OnlineCinemaBooking/BookingSystem.py
@@ -26,6 +26,7 @@ class BookingSystem:
             return f"Sorry {self.movies[self.customer.movie_selected]} seats are sold."
 
     def customer_booking_info(self):
+        pass
         
 
             
